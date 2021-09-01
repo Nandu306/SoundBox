733266a30a30ce096ab3dac1b37641098d22b6fe
Nandu Rajagopala <nandu.rajagopala@cantab.net>
2021-09-01 23:22:45 +0200

    tested out pagination

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,9 +8,13 @@ class SearchController < ApplicationController
       req.params['term'] = params["term"]
       req.params['media'] = "podcast"
       req.params['lang'] = "en_us"
+      # req.params['limit'] = 20
+      # req.params['offset'] = 100
+
       # req.params['explicit'] = "No"
       # req.params['country'] = "GB"
     end
+
     @json = JSON.parse(@response.body)
 
   end
