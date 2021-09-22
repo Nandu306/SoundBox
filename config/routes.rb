43bab7f9bcc679dbbf169b56ca7b4ec8432cdf4d
Nandu Rajagopala <nandu.rajagopala@cantab.net>
2021-09-22 21:39:35 +0200

    modified route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'search#new', as: :home
-  get '/search' => 'search#index'
+  get '/search', to: 'search#index'
   post '/search', to: 'search#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
